f58e9f564a8610e3734a1afb4767ac921f68a955
NIKHIL SINGH <nikhilsingh110104@gmail.com>
2026-02-17 18:51:48 +0530

    Create Welcome .java

---
+++ Java/Welcome .java
@@ -0,0 +1,9 @@
+class Welcome{
+    public static void main(String args[]){
+
+        String name=args[0];
+        String surname=args[1];
+
+        System.out.println("Welcome"+ name +""+ surname+"!");
+    }
+}
