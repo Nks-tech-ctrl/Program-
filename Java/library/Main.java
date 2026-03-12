e9c3a31c73fac040f5f9f0a00dc39856cd35febd
NIKHIL SINGH <nikhilsingh110104@gmail.com>
2026-03-12 06:15:40 +0530

    Add files via upload

---
+++ Java/library/Main.java
@@ -0,0 +1,11 @@
+package library;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Book b = new Book();
+        b.setData(1, "Java", "James Gosling");
+        b.display();
+
+    }
+}
